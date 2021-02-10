356d0c143b64f8658d012109bdbc7cb7f6e549e8
bv-loop <72999113+bv-loop@users.noreply.github.com>
2021-02-10 21:54:05 +0000

    Add Google OAuth and complete project

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 * Users need to register or login in order to access the homepage.
 * Users email and encrypted password is stored on a MongoDB database.
-* User passwords are encrypted with mongoose-encrypt package.
 * Encryption-keys are safely hidden as environment variables.
-* When a user logs-in, firstly their email address gets queried on the database, if there is a match then the binary-encoded password on the DB gets deciphered to JSON, then the JSON is parsed, and the individual fields are inserted back into the document as their original data types, at this point the decoded password gets compared to the password as entered by the user on the website login page, if successfull, the home page will be rendered.
+* Google OAuth2 is used.
+* Cookies and Session Tokens are generated and used in the site.
--- app.js
+++ app.js
@@ -1,87 +1,244 @@
+/////////////////////////////////////////////////////////////
 // REQUIRE PACKAGES
+/////////////////////////////////////////////////////////////
 require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
-
-// CONFIGURE EXPRESS APP
+/////////////////////////////////////////////////////////////
+// CONFIGURE EXPRESS APP AND MIDDLEWARE
+/////////////////////////////////////////////////////////////
 const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+/////////////////////////////////////////////////////////////
 // CONFIGURE MONGODB CONNECTION WITH MONGOOSE
-mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology: true});
+/////////////////////////////////////////////////////////////
+mongodb+srv://admin-bart:<password>@cluster0.ljolo.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose.connect('mongodb+srv://'+ATLAS_ADMIN_USERNAME +':'+ ATLAS_ADMIN_PASSWORD+'@cluster0.ljolo.mongodb.net/userDB', {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
+
+/////////////////////////////////////////////////////////////
 // MONGOOSE SCHEMA AND MODEL CREATION
+/////////////////////////////////////////////////////////////
 // USER SCHEMA
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  secrets: []
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
 
+/////////////////////////////////////////////////////////////
+// PLUGIN PASSPORT-LOCAL-MONGOOSE INTO THE SCHEMA
+/////////////////////////////////////////////////////////////
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+
+/////////////////////////////////////////////////////////////
 // USER COLLECTION/MODEL
+/////////////////////////////////////////////////////////////
 const User = mongoose.model('User', userSchema);
 
 
+/////////////////////////////////////////////////////////////
+// SIMPLIFIED PASSPORT/PASSPORT-LOCAL CONFIGURATION / GOOGLE SESSIONS
+/////////////////////////////////////////////////////////////
+passport.use(User.createStrategy()); //createStrategy is responsible to setup passport-local LocalStrategy with the correct options.
+//creates cookie with unique user id
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+//deletes cookie with the unique user id
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+/////////////////////////////////////////////////////////////
+// GOOGLE AUTHENTICATION CONFIGURE STRATEGY
+/////////////////////////////////////////////////////////////
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+/////////////////////////////////////////////////////////////
 // HOME ROUTE
+/////////////////////////////////////////////////////////////
 app.get("/", function(req, res){
   res.render("home");
 });
 
 
-// LOGIN ROUTE
+/////////////////////////////////////////////////////////////
+// GOOGLE ROUTE TO AUTHENTICATE REQUESTS
+/////////////////////////////////////////////////////////////
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+
+/////////////////////////////////////////////////////////////
+// GOOGLE ROUTE TO HANDLE/REDIRECT SUCCESSFUL AUTHENTICATIONS
+/////////////////////////////////////////////////////////////
+app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect to secrets.
+    res.redirect("/secrets");
+  });
+
+
+/////////////////////////////////////////////////////////////
+// LOGIN ROUTE GET REQUESTS
+/////////////////////////////////////////////////////////////
 app.get("/login", function(req, res){
   res.render("login");
 });
 
-app.post("/login", function(req, res){
-  const email = req.body.username;
-  const password = req.body.password;
 
-  User.findOne({email: email}, function(err, foundUser){
-  if (err){
-    console.log(err);
-  }  else{
-    if (foundUser){
-      if (foundUser.password === password){
-        res.render("secrets");
-      }
-    }
-  }
+/////////////////////////////////////////////////////////////
+// LOGIN ROUTE POST REQUESTS
+/////////////////////////////////////////////////////////////
+app.post("/login", function(req, res){
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password
   });
+
+  req.login(user, function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+
 });
 
 
-// REGISTER ROUTE
+/////////////////////////////////////////////////////////////
+// REGISTER ROUTE GET REQUEST
+/////////////////////////////////////////////////////////////
 app.get("/register", function(req, res){
   res.render("register");
 });
 
+
+/////////////////////////////////////////////////////////////
+// REGISTER ROUTE POST REQUEST
+/////////////////////////////////////////////////////////////
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err){
-    if(err){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err){
       console.log(err);
-    }else{
-      res.render("secrets");
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+
+});
+
+
+/////////////////////////////////////////////////////////////
+// SECRETS ROUTE GET REQUEST
+/////////////////////////////////////////////////////////////
+app.get("/secrets", function(req, res){
+  User.find( {secrets: {$ne:[]}} ,
+    function(err, foundUsers){
+      if (err){
+        console.log(err);
+      } else {
+        if (foundUsers){
+          res.render("secrets", {userWithSecrets: foundUsers, isAuthenticated: req.isAuthenticated()});
+        }
+      }
+    });
+});
+
+
+/////////////////////////////////////////////////////////////
+// SUBMIT ROUTE TO ADD NEW SECRET
+/////////////////////////////////////////////////////////////
+app.get("/submit", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+
+/////////////////////////////////////////////////////////////
+// SUBMIT ROUTE POST REQUESTS
+/////////////////////////////////////////////////////////////
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+  console.log(req.user.id);
+  User.updateOne({_id:req.user.id}, {$push: {secrets: submittedSecret}}, function(err){
+    if (err){
+      console.log(err);
+    } else {
+      res.redirect("/secrets");
     }
   });
 });
 
 
+/////////////////////////////////////////////////////////////
+// LOGOUT ROUTE
+/////////////////////////////////////////////////////////////
+app.get("/logout", function(req, res){
+  req.logout();
+  res.redirect("/");
+});
+
+
+/////////////////////////////////////////////////////////////
 // LAUNCH EXPRESS SERVER
-app.listen(3000, function () {
-    console.log("Server started on port 3000.")
+/////////////////////////////////////////////////////////////
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started succesfully");
 });
